9c63f1138cf166e20a9c24c587304970ec426f41
John Irungu <johnydanxin@gmail.com>
2022-10-27 14:02:29 +0300

    Revert "jetstream and inertia teams installed"
    This reverts commit fc506b1a171c5c6be99c1130c9556d05a37881d3.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,34 @@
-import './bootstrap';
-import '../css/app.css';
+require('./bootstrap');
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+import mitt from 'mitt';
+
+const emitter = mitt();
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
+            .mixin({ methods: { route } })
             .mount(el);
     },
 });
 
+// Vue 3
+/*
+createApp({}).directive("focus", {
+  inserted: (el, binding) => {
+    if (binding.value === true) {
+        el.focus();
+    }
+  },
+});
+*/
 InertiaProgress.init({ color: '#4B5563' });
